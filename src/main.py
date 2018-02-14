7c928c4afc9f080585cc0d499fbb039d50412db9
iacopy <iacopomarmorini@gmail.com>
2018-02-14 14:47:19 +0100

    debug: remove almost everything. numpy is found?

--- src/main.py
+++ src/main.py
@@ -2,19 +2,11 @@
 Just import things.
 """
 
-# Standard Library
-import sys
-
 # 3rd party
 import numpy as np
 
-# My stuff
-from samples import sample_module
 
 if __name__ == '__main__':
     print('numpy', np.version.full_version)
     print(np.__file__)
-    try:
-        print(sample_module.reverse_manually(sys.argv[1]))
-    except IndexError:
-        print('No args?')
+
