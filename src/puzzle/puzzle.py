a79dfff154949175a4a19c0447d74f7b2b3e2284
iacopy <iacopomarmorini@gmail.com>
2018-03-07 22:13:55 +0100

    Revert "code/refactor: puzzle.swap now takes 3 args instead of 4"
    This reverts commit 74507b3a8625b6cc575d305a5f361415a81b2b5b.

--- src/puzzle/puzzle.py
+++ src/puzzle/puzzle.py
@@ -6,10 +6,9 @@ Puzzle functions.
 from copy import copy
 
 
-def swap(ary, size, cells):
+def swap(ary, size, cell_0, cell_1):
     """
-    Given an array `ary`, swap 2 `cells`.
-
+    Given an array `ary`, swap `cell_0` with `cell_1`.
     Each cell is `size` x `size`.
     """
     def iter_cell(cell, axis):
@@ -19,7 +18,6 @@ def swap(ary, size, cells):
         """
         return range(cell[axis] * size, cell[axis] * size + size)
 
-    cell_0, cell_1 = cells
     for y_0, y_1 in zip(iter_cell(cell_0, 0), iter_cell(cell_1, 0)):
         for (x_0, x_1) in zip(iter_cell(cell_0, 1), iter_cell(cell_1, 1)):
             # could not managed to do it without tmp
